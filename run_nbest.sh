944d20f519081dc76f7e9f6f45c08a0d819fceb7
huangruizhe <eraser567@163.com>
2022-05-13 15:21:50 -0400

    updates

--- run_nbest.sh
+++ run_nbest.sh
@@ -7,16 +7,16 @@ stop_stage=10000
 nj=
 
 
-##############################
+############################################################
 # Generate a list of keywords as search terms
-##############################
+############################################################
 if [ ${stage} -le -11 ] && [ ${stop_stage} -ge -11 ]; then
     steps/get_keywords.sh
 fi
 
-##############################
-# Generate nbest list from either Kaldi, ESPNET
-##############################
+############################################################
+# Generate nbest list and get the nbest directory structure
+############################################################
 if [ ${stage} -le -10 ] && [ ${stop_stage} -ge -10 ]; then
     steps/get_nbest_kaldi.sh
     steps/get_nbest_espnet.sh
@@ -27,9 +27,9 @@ if [ ${stage} -le -10 ] && [ ${stop_stage} -ge -10 ]; then
     steps/analysis_cm.sh
 fi
 
-##############################
+############################################################
 # Get time alignment of the 1st best hypothesis
-##############################
+############################################################
 if [ ${stage} -le -9 ] && [ ${stop_stage} -ge -9 ]; then
     steps/get_time_kaldi.sh
     steps/get_time_montreal.sh
@@ -39,18 +39,18 @@ if [ ${stage} -le -9 ] && [ ${stop_stage} -ge -9 ]; then
     steps/analysis_time.sh
 fi
 
-##############################
+############################################################
 # Generate sausage with the nbest list
-##############################
+############################################################
 if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
     score_type="ctc"
     steps/get_confusion_network.sh $score_type
 fi
 
-##############################
-# KWS
-##############################
-if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
+############################################################
+# Perform KWS
+############################################################
+if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
     steps/kws_build_indice_from_cn.sh
     steps/kws_search.sh
 fi
--- steps/get_nbest_espnet.sh
+++ steps/get_nbest_espnet.sh
@@ -2,130 +2,129 @@
 # Copyright (c) 2021, Johns Hopkins University, Ruizhe Huang
 # License: Apache 2.0
 
+# checkout /export/fs04/a12/rhuang/kws/kws/run_espnet.sh
+# TODO: check-in my version of espnet
+
+# begin configuration section.
+cmd=run.pl
+stage=0
+stop_stage=10000
+nj=
+max_states_scale=-1
+max_states=1000000
+skip_optimization=false
+score_type=
+espnet_asr=/export/fs04/a12/rhuang/espnet/egs2/swbd/asr1
+espnet_model="espnet/roshansh_asr_base_sp_conformer_swbd"
+#end configuration section.
+
+cd $espnet_asr
+[ -f ./path.sh ] && . ./path.sh
+. parse_options.sh || exit 1;
+
+# from ESPNet
+log() {
+    local fname=${BASH_SOURCE[1]##*/}
+    echo -e "$(date '+%Y-%m-%dT%H:%M:%S') (${fname}:${BASH_LINENO[0]}:${FUNCNAME[1]}) $*"
+}
+
+# Absolute paths are prefered
+data=$1
+espnet_decode_dir=$2
+nbest_dir=$3
+
+# echo "------------------ Parameters ------------------"
+# echo nbest_dir: $nbest_dir
+# echo data_dir: $data_dir
+# echo indices_dir: $indices_dir
+# echo scale: $scale
+# echo kaldi: $kaldi
+# echo montreal: $montreal
+# echo "------------------------------------------------"
+
+# cd /export/fs04/a12/rhuang/espnet/egs2/swbd/asr12
+
+mkdir -p $nbest_dir/log
+
+[[ -z "$nj" ]] && [[ -d $espnet_decode_dir/logdir ]] && nj=`ls -d1 $espnet_decode_dir/logdir/output.* | wc -l`  # espnet
+log "num_job: $n"
+
 ################################################
 # Get the required nbest dir from espnet's nbest output
 ################################################
 
-cmd=queue.pl
+# cmd=queue.pl
 
-data=std2006_dev
-kws_exp=/export/fs04/a12/rhuang/kws/kws-release/exp/$data/
-nbest_dir=${kws_exp}/nbest_espnet/
-n=100
-# datasets="std2006_dev std2006_eval callhome_dev callhome_eval callhome_train"
+# data=std2006_dev
+# kws_exp=/export/fs04/a12/rhuang/kws/kws-release/exp/$data/
+# nbest_dir=${kws_exp}/nbest_espnet/
+# n=100
+# # datasets="std2006_dev std2006_eval callhome_dev callhome_eval callhome_train"
 
-espnet_asr=/export/fs04/a12/rhuang/espnet/egs2/swbd/asr1
+# espnet_asr=/export/fs04/a12/rhuang/espnet/egs2/swbd/asr1
 
-decode_dir=${espnet_asr}/exp/espnet/roshansh_asr_base_sp_conformer_swbd/decode_asr_nbest100_lm_lm_train_lm_bpe2000_valid.loss.best_asr_model_valid.acc.ave/${data}/
-# data_dir=data/${data}
+# espnet_decode_dir=${espnet_asr}/exp/espnet/roshansh_asr_base_sp_conformer_swbd/decode_asr_nbest100_lm_lm_train_lm_bpe2000_valid.loss.best_asr_model_valid.acc.ave/${data}/
+# # data_dir=data/${data}
+
+################################################
+# Data prep
+################################################
+if [ ${stage} -le -10 ] && [ ${stop_stage} -ge -10 ]; then
+    # utils/copy_data_dir.sh /export/fs04/a12/rhuang/espnet/egs2/swbd/asr12/data/$data/ data/$data
+
+    # Make features
+    bash run.sh --skip_data_prep false \
+    --skip_train true \
+    --download_model $espnet_model \
+    --stage 3 \
+    --stop_stage 3 \
+    --train_set $data --valid_set "None" --test_sets "None"
+fi
 
 ################################################
 # Decode the nbest list from espnet's model
 ################################################
+if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
+    # make sure that in "espnet2/bin/asr_inference.py" the "token_list" is correct
 
-# TODO: check-in my version of espnet
+    bash run.sh --download_model $espnet_model \
+        --test_sets "$data" --skip_data_prep true --skip_train true \
+        --inference_args "--nbest $n" --stop_stage 13
 
-# /export/fs04/a12/rhuang/kws/kws/run_espnet.sh
+    # get kaldi's style WER
+    # ref=/export/fs04/a12/rhuang/kws/kws_exp/shay/s5c/exp/chain/tdnn7r_sp/decode_${data}_sw1_fsh_fg_rnnlm_1e_0.45/scoring_kaldi/test_filt.txt
+    # hyp=/export/fs04/a12/rhuang/espnet/egs2/swbd/asr1/exp/espnet/roshansh_asr_base_sp_conformer_swbd/decode_asr_nbest100_lm_lm_train_lm_bpe2000_valid.loss.best_asr_model_valid.acc.ave/${data}/text
+    # bash local/score_kaldi_light.sh $ref $hyp data/$data $(dirname "$hyp")
 
-cd $espnet_asr
-conda activate espnet_gpu
+    # Other decoding options:
+    # E.g. run ctc decoding
+    inference_tag="decode_asr_nbest100_valid.loss.best_asr_model_valid.acc.ave_withoutlm"
+    bash run.sh --test_sets "$data" \
+        --skip_data_prep true --skip_train true \
+        --download_model $espnet_model \
+        --inference_tag ${inference_tag} \
+        --stop_stage 13 \
+        --inference_config "conf/decode_asr_ctc.yaml"
+    
+    # E.g. run without transformer lm
+    # conf/decode_asr_nolm.yaml
+fi
 
-# first go to "espnet2/bin/asr_inference.py" to modify about "token_list"
-bash run.sh --skip_data_prep true \
-    --skip_train true \
-    --download_model espnet/roshansh_asr_base_sp_conformer_swbd \
-    --stop_stage 12
-
-# debugged for 5 hours to finally being able to run the model
-
-# make features
-utils/copy_data_dir.sh /export/fs04/a12/rhuang/espnet/egs2/swbd/asr12/data/std2006_dev/ data/std2006_dev
-utils/copy_data_dir.sh /export/fs04/a12/rhuang/espnet/egs2/swbd/asr12/data/std2006_eval/ data/std2006_eval
-# d=train; utils/copy_data_dir.sh /export/fs04/a12/rhuang/espnet/egs2/swbd/asr12/data/callhome_${d} data/callhome_${d}
-d=dev; utils/copy_data_dir.sh /export/fs04/a12/rhuang/espnet/egs2/swbd/asr12/data/callhome_${d} data/callhome_${d}
-d=eval; utils/copy_data_dir.sh /export/fs04/a12/rhuang/espnet/egs2/swbd/asr12/data/callhome_${d} data/callhome_${d}
-
-# for callhome path
-cd /export/fs04/a12/rhuang/espnet/egs2/swbd/asr1
-ln -s /export/corpora5/LDC/LDC97S42/ .
-
-vi run.sh
-# set the following:
-test_sets="std2006_dev std2006_eval callhome_dev callhome_eval"
-# modify "Stage 3" in asr.sh to skip train_set, valid_set
-# then run:
-bash run.sh --skip_data_prep false \
-    --skip_train true \
-    --download_model espnet/roshansh_asr_base_sp_conformer_swbd \
-    --stage 3 \
-    --stop_stage 3
-
-# decoding
-# To save time, it is better to run each dataset on one machine
-pretrained="espnet/roshansh_asr_base_sp_conformer_swbd"
-test_set=
-bash run.sh --test_sets "$test_set" \
-    --skip_data_prep true --skip_train true \
-    --download_model $pretrained \
-    --stop_stage 12 # --gpu_inference true
-
-# scoring
-test_sets="std2006_dev std2006_eval"
-bash run.sh --test_sets "$test_sets" \
-    --skip_data_prep true --skip_train true \
-    --download_model $pretrained \
-    --stage 13 --stop_stage 13
-
-# get nbest
-n=100
-test_sets=std2006_dev
-test_sets=std2006_eval
-test_sets=callhome_dev
-test_sets=callhome_eval
-bash run.sh --download_model $pretrained \
-    --test_sets "$test_sets" --skip_data_prep true --skip_train true \
-    --inference_args "--nbest $n" --stop_stage 12
-
-# get WER
-data=$test_sets
-ref=/export/fs04/a12/rhuang/kws/kws_exp/shay/s5c/exp/chain/tdnn7r_sp/decode_${data}_sw1_fsh_fg_rnnlm_1e_0.45/scoring_kaldi/test_filt.txt
-datadir=data/${data}/
-decode=exp/espnet/roshansh_asr_base_sp_conformer_swbd/decode_asr_nbest100_lm_lm_train_lm_bpe2000_valid.loss.best_asr_model_valid.acc.ave/${data}/
-hyp=$decode/text
-bash local/score_kaldi_light.sh $ref $hyp $datadir $decode
-
-# run without transformer lm
-# edit run.sh to use "inference_config=conf/decode_asr_nolm.yaml"
-inference_tag="decode_asr_nbest100_valid.loss.best_asr_model_valid.acc.ave_withoutlm"
-test_sets=eval2000
-bash run.sh --test_sets "$test_sets" \
-    --skip_data_prep true --skip_train true \
-    --download_model $pretrained \
-    --inference_tag ${inference_tag} \
-    --stop_stage 12
-
-# run ctc decoding
-# edit run.sh to use "inference_config=conf/decode_asr_ctc.yaml"
-inference_tag="decode_asr_nbest100_valid.loss.best_asr_model_valid.acc.ave_ctc"
-test_sets=eval2000
-bash run.sh --test_sets "$test_sets" \
-    --skip_data_prep true --skip_train true \
-    --download_model $pretrained \
-    --inference_tag ${inference_tag} \
-    --stop_stage 12
-
-
-# scoring
-test_sets=eval2000
-bash run.sh --test_sets "$test_sets" \
-    --skip_data_prep true --skip_train true \
-    --download_model $pretrained \
-    --inference_tag ${inference_tag} \
-    --stage 13 --stop_stage 13
 
 ################################################
 # Convert ESPNet's nbest format to our format
 ################################################
+
+if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
+    # First, Obtain word-level confidence scores from token-level scores
+    log "score_type: $score_type"
+    log "log path: $nbest_dir/log/wscores.1.log"
+    time $cmd JOB=1:$nj $nbest_dir/log/wscores.JOB.log \
+        set -e -o pipefail '&&' \
+        mkdir -p $nbest_dir/temp/JOB/ '&&' \
+        bash $func get_w_scores JOB $length_bonus $nbest_dir $score_type
+    grep -iF error $nbest_dir/log/wscores.*.log
+fi
 
 cd $espnet_asr
 conda activate espnet_gpu
